b595e4a19631ab092b1438eeb41b66ce39759010
Michael Huynh <mxh@mit.edu>
2011-08-05 19:00:38 -0400

    Fixed bugs in redirect.

--- sso/views.py
+++ sso/views.py
@@ -3,6 +3,7 @@ from django.template import RequestContext
 from django.http import HttpResponse
 
 from cas_provider.forms import LoginForm
+from cas_provider.utils import create_service_ticket
 
 import re
 
@@ -17,7 +18,8 @@ def login(request, template_name='cas/login.html', success_redirect='/accounts/'
         try:
             email = request.META['SSL_CLIENT_S_DN_Email']
             #Let's pull out the username part:
-            r = re.match(r'(\w+)@mit.edu', email)
+            r = re.match(r'(\w+)@mit.edu', email, re.IGNORECASE)
+            #return HttpResponse(email)
 
             if email != 'scripts@mit.edu' and r:
                 #Success!
@@ -26,19 +28,20 @@ def login(request, template_name='cas/login.html', success_redirect='/accounts/'
 
                 if service.find('?') == -1:
                     return redirect(service + '?ticket=' + ticket.ticket)
-                else:
-                    return redirect(service + '&ticket=' + ticket.ticket)
+                return redirect(service + '&ticket=' + ticket.ticket)
 
         except KeyError:
             #If that key isn't found, then it means that we are not in SSL mode.
             #Redirect the user to port :444 (assuming this is 
             #http://username.scripts.mit.edu/)
             url = 'https://%s:%i%s' % (request.META['SERVER_NAME'], 444,
-                                       request.path)
-            redirect(url)
+                                       request.get_full_path())
+            return redirect(url)
 
 
     except KeyError:
         #TODO: Redirect to placeholder page explaining the service.
         #return redirect(success_redirect)
         return HttpResponse('service key must be defined!')
+    
+    return HttpResponse('error')
